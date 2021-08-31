4cebddc870f5421dd59d93c854f1347f803bfa63
QuanKhs <quanphamluong@gmail.com>
2021-08-31 15:41:08 +0700

    start apollo server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const typeDefs = require('./schemas/schema');
 const resolvers = require('./resolver/resolver');
 
-
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
 
-const app = express();
-server.applyMiddleware({app});
+const startApolloServer = async (typeDefs, resolvers) => {
+    const app = express();
 
-app.listen({port: 4000}, () => {
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-});
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () => {
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    });
+};
+
+startApolloServer(typeDefs, resolvers);
+
+
+
+
+
