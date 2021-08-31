713f779517a703c8738deb66b6ad0ee1bb48934e
QuanKhs <quanphamluong@gmail.com>
2021-08-31 15:28:47 +0700

    express server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const typeDefs = require('./schemas/schema');
+const resolvers = require('./resolver/resolver');
+
+
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+server.applyMiddleware({app});
+
+app.listen({port: 4000}, () => {
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+});
